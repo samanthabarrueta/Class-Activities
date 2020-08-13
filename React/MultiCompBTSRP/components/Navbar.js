f1745cb3578156d3438191176237e6deab8a1fb5
Samantha <samanthabarrueta@gmail.com>
2020-08-13 00:17:44 -0500

    react variables/bootstrap

---
+++ React/MultiCompBTSRP/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (<div className="container">
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <a className="navbar-brand" href="/">
+      Navbar
+    </a></nav></div>)
+}
+
+export default Navbar
---
+++ React/ReactVariables/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Jumbotron from "./components/Jumbotron";
+import Card from "./components/Card";
+
+const App = () => {
+  return (<div><Navbar />
+    <Jumbotron />
+    <Card /></div>)
+}
+
+export default App;
